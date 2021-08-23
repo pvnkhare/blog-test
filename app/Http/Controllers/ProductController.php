f487cdfccb0f9dc2e290de4995cf558f1e9d412c
pvn <pvn>
2021-08-23 17:15:32 +0530

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use DataTables;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Product::latest()->get();
+            return Datatables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action',function($row){
+                $btn = '<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct"><i class="fas fa-pen text-white"></i></a>';
+                $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct"><i class="far fa-trash-alt text-white" data-feather="delete"></i></a>';
+
+                return $btn;
+
+            })
+            ->rawColumns(['action'])->make(true);
+
+        }
+
+        return view('productajax');
+    }   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Product $product)
+    {
+        $input = $request->all();
+//dd($input['id']);
+        if($input['id'] != null){
+            $product->where('id', $input['id'])->update($input); 
+        }else{
+            Product::updateOrCreate($input); 
+        }
+     //   Product::updateOrCreate($input);        
+   
+        return response()->json(['success'=>'Product saved successfully.']);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::find($id)->delete();
+     
+        return response()->json(['success'=>'Product deleted successfully.']);
+    }
+}
